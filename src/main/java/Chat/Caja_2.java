e15c22147a2768ba4db28013d0a7a0283893964c
LPCS <l_pablocs@hotmail.com>
2021-08-26 22:58:12 -0500

    ConclusiónDeLógica

--- src/main/java/Chat/Caja_2.java
+++ src/main/java/Chat/Caja_2.java
@@ -5,7 +5,6 @@
  */
 package Chat;
 
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -16,6 +15,7 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class Caja_2 {
+
     //Variables estaticas para poder hacer la conexion cliente-servidor
     static Socket puerto;
     static Socket puerto2;
@@ -32,55 +32,101 @@ public class Caja_2 {
     static String ip = "0";
 //Variable estatica que permite pasar como parametro la ip a la clase Usuario
     CrearUsuario Nombre1 = new CrearUsuario(Nombre);//Creo un objeto de la clase AsignarIpNombre y le paso el nombre
-    CrearUsuario direccion = new CrearUsuario(Nombre,ip);//Creo un objeto de la clase AsignarIpNombre y le paso el nombre junto con la Ip
+    CrearUsuario direccion = new CrearUsuario(Nombre, ip);//Creo un objeto de la clase AsignarIpNombre y le paso el nombre junto con la Ip
     //UsuarioCliente tipo = new UsuarioCliente();//Objeto de la clase TipodeUsuario que indica si el inicializado es cliente o servidor
     //Variable del tipo string que almacena el chat
     static String historial = "";
     public JFrame f = new JFrame("Caja_2");
     final JLabel label = new JLabel();
+    final JLabel lValor = new JLabel();
+    final JLabel lPeso = new JLabel();
+    final JLabel lImpuesto = new JLabel();
+    public int valorProducto;
+    public int pesoProducto;
+    public int impuestoProducto;
     public String articulos[] = {"Audifonos", "Celular", "Impresora", "Monitor", "Mouse", "Parlante", "Teclado"};
     public JComboBox cb = new JComboBox(articulos);
     JButton botonenviar = new JButton("Enviar");
     JButton botonSalir = new JButton("Salir");
     static boolean salir = true;
-        public void GUI() {
-            ip = direccion.getIp();
-            label.setHorizontalAlignment(JLabel.CENTER);
-            label.setSize(400, 100);
-            cb.setBounds(50, 50, 90, 20);
-            f.setSize(300, 300);
-            f.setLayout(null);
-            f.setVisible(true);
-            botonenviar.setBounds(50, 100, 75, 20);
-            botonSalir.setBounds(150, 100, 75, 20);
-            f.add(cb);
-            f.add(label);
-            f.add(botonenviar);
-            f.add(botonSalir);
-            botonenviar.setText("Enviar");
-            botonenviar.addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
-                    botonenviarActionPerformed(evt);
-                }
-            });
 
-}
-        
+    public void GUI() {
+        ip = direccion.getIp();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setSize(400, 100);
+        cb.setBounds(50, 50, 90, 20);
+        f.setSize(300, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+        botonenviar.setBounds(50, 100, 75, 20);
+        botonSalir.setBounds(150, 100, 75, 20);
+        f.add(cb);
+        f.add(label);
+        f.add(botonenviar);
+        f.add(botonSalir);
+        botonenviar.setText("Enviar");
+        botonenviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonenviarActionPerformed(evt);
+            }
+        });
+
+    }
+
     private void botonenviarActionPerformed(java.awt.event.ActionEvent evt) {
         //ENVIO DEL MENSAJE AL SERVIDOR
         try {
-            String mensajesalida = "";//Declaracion de la variable que almacena el mensaje que será enviado
-            mensajesalida = ""+cb.getSelectedIndex();//Declaracion de la variable que almacena el mensaje que será enviado
-            datossalida.writeUTF(""+ mensajesalida);//se envia el mensaje del cliente al server codificado
-            datossalida2.writeUTF(""+ mensajesalida);
-            datossalida3.writeUTF(""+ mensajesalida);
+            //String mensajesalida = "" + cb.getSelectedIndex();
+            asignar_valores(cb.getSelectedIndex());
+            datossalida.writeUTF("" + valorProducto);
+            datossalida2.writeUTF("" + pesoProducto);
+            datossalida3.writeUTF("" + impuestoProducto);
         } catch (Exception e) {
 
         }
 
-    }     
+    }
+
     public void cambio(String str) {
         label.setText(str);
+    }
+
+    public void asignar_valores(int cod) {
+        if (cod == 0) {
+            valorProducto = 5000;
+            pesoProducto = 27;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 1) {
+            valorProducto = 113000;
+            pesoProducto = 269;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 2) {
+            valorProducto = 81000;
+            pesoProducto = 3000;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 3) {
+            valorProducto = 137000;
+            pesoProducto = 7500;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 4) {
+            valorProducto = 9000;
+            pesoProducto = 137;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 5) {
+            valorProducto = 23000;
+            pesoProducto = 2750;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
+        if (cod == 6) {
+            valorProducto = 41000;
+            pesoProducto = 790;
+            impuestoProducto = (int) (valorProducto * 0.13);
+        }
     }
 
     public String selec() {
@@ -90,18 +136,17 @@ public class Caja_2 {
     public int posicion() {
         return cb.getSelectedIndex();
     }
+
     public static void main(String[] args) {
         Caja_2 caja = new Caja_2();
         caja.GUI();
-        
-    /**    while (true) {
-            String data = "" + caja.posicion();
-            caja.cambio(data);
-        }
-      */  
-     try{
-           
-            
+
+        /**
+         * while (true) { String data = "" + caja.posicion(); caja.cambio(data);
+         * }
+         */
+        try {
+
             puerto = new Socket(ip, 1201);//Se recibe el valor de la ip y se inicializa el puerto
             puerto2 = new Socket(ip, 1203);//Se recibe el valor de la ip y se inicializa el puerto
             puerto3 = new Socket(ip, 1205);
@@ -112,15 +157,20 @@ public class Caja_2 {
             datossalida2 = new DataOutputStream(puerto2.getOutputStream());
             datosentrada3 = new DataInputStream(puerto3.getInputStream());
             datossalida3 = new DataOutputStream(puerto3.getOutputStream());
-            String mensaje="";//Declaracion de la variable en la que se va a almacenar el mensaje
-            while(salir){
-                mensaje = datosentrada.readUTF()+datosentrada2.readUTF()+datosentrada3.readUTF();//Lee y decodifica el mensaje que le ha sido enviado
+            String mensaje = "";//Declaracion de la variable en la que se va a almacenar el mensaje
+            while (salir) {
+                int a = Integer.parseInt(datosentrada.readUTF());
+                int b = Integer.parseInt(datosentrada2.readUTF());
+                int c = Integer.parseInt(datosentrada3.readUTF());
+                int monto = (int) (a + c + (b * 0.15));
+                // mensaje = datosentrada.readUTF()+" "+datosentrada2.readUTF()+" "+datosentrada3.readUTF();//Lee y decodifica el mensaje que le ha sido enviado
+                mensaje = "" + monto;
                 caja.cambio(mensaje);
             }
-        
-        }catch (Exception e){
-        
+
+        } catch (Exception e) {
+
         }
     }
-    
+
 }
